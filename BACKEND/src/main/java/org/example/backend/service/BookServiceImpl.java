71e2559b04d08d2d0e2153237c98464911767672
virat virat18 <tejagudditi@gmail.com>
2026-05-26 23:13:10 +0530

    Error handling

--- BACKEND/src/main/java/org/example/backend/service/BookServiceImpl.java
+++ BACKEND/src/main/java/org/example/backend/service/BookServiceImpl.java
@@ -9,7 +9,7 @@ import org.example.backend.repository.BookRepository;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
-import java.util.List;
+import java.util.*;
 import java.util.UUID;
 
 @Service
