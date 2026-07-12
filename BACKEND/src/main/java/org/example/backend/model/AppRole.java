0285c1fb83034c7a1c27d25088bcaa046579fd33
virat virat18 <tejagudditi@gmail.com>
2026-07-12 23:49:53 +0530

    user registration

--- BACKEND/src/main/java/org/example/backend/model/AppRole.java
+++
@@ -1,6 +0,0 @@
-package org.example.backend.model;
-
-public enum AppRole {
-    ROLE_USER,
-    ROLE_ADMIN
-}
--- BACKEND/src/main/java/org/example/backend/model/Role.java
+++ BACKEND/src/main/java/org/example/backend/model/Role.java
@@ -1,25 +1,20 @@
 package org.example.backend.model;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
+@Table(name="roles")
 @Getter
 @Setter
-@AllArgsConstructor
-@NoArgsConstructor
-@Table(name="roles")
 public class Role {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    private Integer id;
 
     @Enumerated(EnumType.STRING)
-    @Column(length=20, unique=true,nullable = false)
-    private AppRole name;
-
+    @Column(length=20)
+    private ERole name;
 }
