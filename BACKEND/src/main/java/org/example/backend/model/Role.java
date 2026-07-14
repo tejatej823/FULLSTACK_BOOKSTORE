08cbb98ea8f6ba3d95c1bf8e22621a6c3d50d23a
virat virat18 <tejagudditi@gmail.com>
2026-07-14 12:35:13 +0530

    user registration

--- BACKEND/src/main/java/org/example/backend/model/Role.java
+++ BACKEND/src/main/java/org/example/backend/model/Role.java
@@ -12,7 +12,7 @@ public class Role {
 
     @Id
     @GeneratedValue(strategy= GenerationType.IDENTITY)
-    private Integer id;
+    private Long id;
 
     @Enumerated(EnumType.STRING)
     @Column(length=20)
---
+++ BACKEND/src/main/java/org/example/backend/repository/RoleRepository.java
@@ -0,0 +1,8 @@
+package org.example.backend.repository;
+
+import org.example.backend.model.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoleRepository extends JpaRepository<Role,Long> {
+
+}
