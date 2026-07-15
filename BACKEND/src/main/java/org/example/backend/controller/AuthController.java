129da2f731f1f3b110ecccf4c61ff9595e864fc1
virat virat18 <tejagudditi@gmail.com>
2026-07-15 14:41:36 +0530

    user registration

---
+++ BACKEND/src/main/java/org/example/backend/controller/AuthController.java
@@ -0,0 +1,21 @@
+package org.example.backend.controller;
+
+
+import org.apache.coyote.Response;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/auth")
+public class AuthController {
+
+    @PostMapping("/register")
+    public ResponseEntity<?> registerUser(){
+
+
+
+        return null;
+    }
+}
