80a4c5b6341313c6118efd6ec4da8c3c69a9160e
virat virat18 <tejagudditi@gmail.com>
2026-07-16 13:12:32 +0530

    user registration

---
+++ BACKEND/src/main/java/org/example/backend/dto/request/UserRequestDto.java
@@ -0,0 +1,29 @@
+package org.example.backend.dto.request;
+
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserRequestDto {
+
+    @NotBlank(message="username is required")
+    @Size(min=1,max=100,message = "username must be between 1 and 100 characters")
+    private String username;
+
+    @NotBlank(message="email is required")
+    @Email(message="enter valid email")
+    private String email;
+
+    @NotBlank(message="password is required")
+    private String password;
+
+}
---
+++ BACKEND/src/main/java/org/example/backend/dto/response/UserResponseDto.java
@@ -0,0 +1,5 @@
+package org.example.backend.dto.response;
+
+public class UserResponseDto {
+
+}
